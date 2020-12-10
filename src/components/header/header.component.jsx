dbd1954c1035c77d4fb3e177fe3b7650f2a4983f
unknown <developerprog@yahoo.comconfig --global user.name>
2020-12-10 16:54:30 +0330

    toggledown hidden or not

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
-
+import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
       <Link className='logo-container' to='/'>
           <Logo className='logo' />
@@ -24,10 +26,19 @@ const Header = ({ currentUser }) => (
               :
               <Link className='option' to='/signin'>SIGN IN</Link>
            }
+           <CartIcon />
        </div>
+       {
+          hidden ? null : <CartDropdown />
+       }
+       
     </div>
 
-)
+);
 
+  const mapStateToProps = ({user: {currentUser}, cart: { hidden } }) => ({
+     currentUser,
+     hidden
+  });
 
-export default Header;
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const CartActionTypes = {
+    TOGGLE_CART_HIIDEN: 'TOGGLE_CART_HIIDEN'
+}
+
+export default CartActionTypes;
